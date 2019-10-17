e3bf5e045fdfd5db7ed6573090d88b9971fe9590
m.malofeev <m.malofeev@avia-centr.ru>
2019-10-17 16:17:45 +0300

    templates

--- resources/views/compositions.blade.php
+++ resources/views/compositions.blade.php
@@ -70,6 +70,48 @@
         <a href="/products/composition5" class="button expanded">Выбрать</a>
     </div>
 
+    <div class="column">
+        <a href="/products/composition6">
+            <img class="thumbnail product-img" src="/image/composition/6.jpg">
+        </a>
+        <h5>Триптих на холсте "Love Story"</h5>
+        <p>3950 руб</p>
+        <a href="/products/composition6" class="button expanded">Выбрать</a>
+    </div>
+
+    <div class="column">
+        <a href="/products/composition7">
+            <img class="thumbnail product-img" src="/image/composition/7.jpg">
+        </a>
+        <h5>Панно из 4х фото-картин на холсте</h5>
+        <p>5416 руб</p>
+        <a href="/products/composition7" class="button expanded">Выбрать</a>
+    </div>
+
+    <div class="column">
+        <a href="/products/composition8">
+            <img class="thumbnail product-img" src="/image/composition/8.jpg">
+        </a>
+        <h5>Композиция из 6и фото-картин на холсте</h5>
+        <p>8952 руб</p>
+        <a href="/products/composition8" class="button expanded">Выбрать</a>
+    </div>
+
+</div>
+
+<hr>
+
+<div class="row small-up-2 large-up-4">
+    <div class="column">
+        <a href="/products/composition9">
+            <img class="thumbnail product-img" src="/image/composition/9.jpg">
+        </a>
+        <h5>Панно из 7и фото-картин на холсте</h5>
+        <p>8450 руб</p>
+        <a href="/products/composition9" class="button expanded">Выбрать</a>
+    </div>
+
+
 </div>
 
 @include('footer')
