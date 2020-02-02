da2a1ace97003a5564280b415b49d53cb0ccbc1d
m.malofeev <m.malofeev@avia-centr.ru>
2020-02-02 12:05:01 +0300

    COUNTER

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -57,3 +57,5 @@
     VK.Widgets.CommunityMessages("vk_community_messages", 47050792, {expanded: "0",tooltipButtonText: "Готовы принять заказ онлайн. Сообщите в чат вид заказа, свои контактные данные для связи, можно приложить свои фото к заказу", "group_id":47050792-127607773, "welcomeScreen":0});
 </script>
 
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(57314767, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/57314767" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+
