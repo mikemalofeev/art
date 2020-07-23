4f486d9df7af9096a642bb1080e45f7ca086ac41
m.malofeev <m.malofeev@avia-centr.ru>
2020-07-23 12:19:52 +0300

    fotosalon24 logo

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -3,7 +3,7 @@
 <meta name="description"
       content="Фотосалон24 - печать фото на холсте. Более 12 различных стилей оформления. Быстрое изготовление и доставка.">
 <meta name="keywords"
-      content="Фотосалон24, печать на холсте, фото, печать фото, фото на холсте, печать на кружках, печать на футболках">
+      content="Фотосалон24, печать на холсте, фото, печать фото, фото на холсте, печать на кружках, печать на футболках, типография в севастополе, печать фото на холсте">
 <title>Фотосалон24 | Печать на холсте</title>
 <link rel="shortcut icon" href="/image/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
