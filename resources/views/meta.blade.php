f2a170e0e330c8bca38335df76ac34383a1bbab0
m.malofeev <m.malofeev@avia-centr.ru>
2020-12-30 14:44:45 +0300

    meta

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -1,10 +1,10 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta name="description"
-      content="Фотосалон24 - печать фото на холсте. Более 12 различных стилей оформления. Быстрое изготовление и доставка.">
+      content="Фотосалон24 - печать фото на холсте. Типография в Севастополе. Более 12 различных стилей оформления. Быстрое изготовление и доставка.">
 <meta name="keywords"
       content="Фотосалон24, печать на холсте, фото, печать фото, фото на холсте, печать на кружках, печать на футболках, типография в севастополе, печать фото на холсте">
-<title>Фотосалон24 | Печать на холсте</title>
+<title>Фотосалон24 | Печать фото на холсте | Типография в Севастополе</title>
 <link rel="shortcut icon" href="/image/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
 <link rel="stylesheet" href="/css/ecommerce.css">
